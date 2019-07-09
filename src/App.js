e235410a998f58cf51842ad9b5f1f6ab159deb5c
Umar--Khan <umarkhandev@gmail.com>
2019-07-09 16:03:38 +0100

    forgot this

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
 import Posts from "./components/Posts";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Posts />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Posts />
+    </div>
+  );
+};
 
 export default App;
